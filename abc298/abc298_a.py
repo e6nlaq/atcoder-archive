5a4125b8190600dfda98f1277d5955fd9267d09e
AutoCloneBot <acautoclone@bot.com>
2023-04-19 14:47:46 +0000

    Added my codes from AtCoder

--- abc298/abc298_a.py
+++ abc298/abc298_a.py
@@ -1,11 +1,26 @@
 def main():
-    N = int(input())
-    S = input()
+  N = int(input())
+  S = input()
 
-    if S.count("o") >= 1 and S.count("x") == 0:
-        print("Yes")
-    else:
-        print("No")
+  # Check if at least one interviewer rated the score as "good".
+  good = False
+  for i in range(N):
+    if S[i] == "o":
+      good = True
+      break
 
+  # Check if at least one interviewer rated him or her as "not acceptable".
+  bad = False
+  for i in range(N):
+    if S[i] == "x":
+      bad = True
+      break
 
-main()
+  # If both of the following two conditions are satisfied, then Takahashi passed; otherwise, he failed.
+  if good and not bad:
+    print("Yes")
+  else:
+    print("No")
+
+if __name__ == "__main__":
+  main()
