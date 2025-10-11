1102ac64022f994d6170ceaa502e67cd22a3b528
AutoCloneBot <acautoclone@bot.com>
2025-10-11 14:51:37 +0000

    Added my codes from AtCoder

---
+++ typical90/typical90_t.py
@@ -0,0 +1,26 @@
+#------------------------------------------------------------
+#
+#
+#	   Welcome to my program!
+#	   DON'T HACK PLEASE!!!!!!!!
+#
+#	　　∧＿∧        AtCoder / Codeforces
+#	　 ( 　･ω･)
+#	＿(__つ/￣￣￣ /  
+#	　　＼/　　　　 /  Python 3.10.12
+#	　　　　￣￣￣￣￣
+#		   Let's write Code!
+#
+#
+#------------------------------------------------------------
+
+import numpy as np
+import math
+
+def main():
+    a,b,c=map(int,input().split())
+    print("Yes" if a < c**b else "No")
+
+
+if __name__ == '__main__':
+    main()
