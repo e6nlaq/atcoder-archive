55d35ade0305f84fcf7904d1a1350300a36326f1
AutoCloneBot <acautoclone@bot.com>
2026-01-04 14:51:39 +0000

    Added my codes from AtCoder

---
+++ abc185/abc185_c.py
@@ -0,0 +1,25 @@
+#------------------------------------------------------------
+#
+#
+#	   Welcome to my program!
+#	   DON'T HACK PLEASE!!!!!!!!
+#
+#	　　∧＿∧        AtCoder / Codeforces
+#	　 ( 　･ω･)
+#	＿(__つ/￣￣￣ /  
+#	　　＼/　　　　 /  Python 3.10.12
+#	　　　　￣￣￣￣￣
+#		   Let's write Code!
+#
+#
+#------------------------------------------------------------
+
+import numpy as np
+import math
+
+def main():
+    print(math.comb(int(input())-1,11))
+
+
+if __name__ == '__main__':
+    main()
